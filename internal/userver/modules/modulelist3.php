dd47313139a7d1b859b0a3a11cf91975358f4f79
Stefan <sea75300@yahoo.de>
2015-07-27 21:03:02 +0200

    Update Modul-Repo

--- internal/userver/modules/modulelist3.php
+++ internal/userver/modules/modulelist3.php
@@ -20,14 +20,35 @@
         'nkorg/modulecreator' => array(
             'name'          => 'FanPress CM 3.x Modulecreator Module',
             'description'   => 'This module allows you to create a base structure for a new module including events, config files, folder structure and so on.',
-            'version'       => '1.0.0',
+            'version'       => '3.0.0',
             'minsysverion'  => '2.99.0',
             'maxsysverion'  => '4.0.0'
         ),
         'nkorg/integration' => array(
             'name'          => 'FanPress CM Integration',
             'description'   => 'Helper module for integration of FanPress CM into your website.',
-            'version'       => '0.0.1',
+            'version'       => '3.0.0',
+            'minsysverion'  => '2.99.0',
+            'maxsysverion'  => '4.0.0'
+        ),
+        'nkorg/tweetextender' => array(
+            'name'          => 'Tweet Extender',
+            'description'   => 'The Tweet Extender allows you to modify posts send to twitter when the connection and sending on creation/ updating is enabled.',
+            'version'       => '3.0.0',
+            'minsysverion'  => '2.99.0',
+            'maxsysverion'  => '4.0.0'
+        ),
+        'nkorg/sitemaplinklist' => array(
+            'name'          => 'sitemap.xml Link List',
+            'description'   => 'Module for use with sitemap.xml files to fill link list in TinyMCE or Link auto completion in HTML-Editor with internal link.',
+            'version'       => '3.0.0',
+            'minsysverion'  => '2.99.0',
+            'maxsysverion'  => '4.0.0'
+        ),
+        'nkorg/support' => array(
+            'name'          => 'FanPress CM Support Module',
+            'description'   => 'This module is a helper module for support cases. It will add a new user account to your system on setup. Only install this module when you are asked to do so!',
+            'version'       => '3.0.0',
             'minsysverion'  => '2.99.0',
             'maxsysverion'  => '4.0.0'
         ),
