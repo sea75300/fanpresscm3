43c1da6f50319a2b72ae7e1bfe5e9ef68c1d0312
Stefan <sea75300@yahoo.de>
2015-08-07 20:38:04 +0200

    Release FPCM 3.0.1

--- internal/userver/system/versionlist3.php
+++ internal/userver/system/versionlist3.php
@@ -1,10 +1,10 @@
 <?php
-    $newversion    = '3.0.0 ';
-    $newversionDev = '3.0.0';
+    $newversion    = '3.0.1';
+    $newversionDev = '3.0.1';
     
     $versions = array(
         'default' => array(
-            'file'      => 'http://nobody-knows.org/updatepools/fanpress/system/packages/fanpress_update_version3.0.0.zip',
+            'file'      => 'http://nobody-knows.org/updatepools/fanpress/system/packages/fanpress_update_version3.0.1.zip',
             'force'     => 0,
             'message'   => 'Frist release of FanPress CM 3.x!',
             'notice'    => 'http://nobody-knows.org/fanpress-cm-3-veroeffentlicht/'
