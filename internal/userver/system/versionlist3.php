3de9146aabfc437cb16e9ba1e19c4c0a2f5288a4
Stefan <sea75300@yahoo.de>
2015-11-01 00:26:12 +0100

    Release FPCM 3.1.0

--- internal/userver/system/versionlist3.php
+++ internal/userver/system/versionlist3.php
@@ -4,7 +4,10 @@
     
     $installfile   = 'http://nobody-knows.org/updatepools/fanpress/system/packages/fanpress3.1.0_full.zip';
     
-    $signature = (file_exists(__DIR__.'/packages/fanpress_update_version3.1.0.zip') ? '$sig$'.md5_file(__DIR__.'/packages/fanpress_update_version3.1.0-rc5.zip').'_'.sha1_file(__DIR__.'/packages/fanpress_update_version3.1.0.zip').'$sig$' : '');
+    $signature = (file_exists(__DIR__.'/packages/fanpress_update_version3.1.0.zip')
+               ? '$sig$'.md5_file(__DIR__.'/packages/fanpress_update_version3.1.0.zip').'_'
+                        .sha1_file(__DIR__.'/packages/fanpress_update_version3.1.0.zip').'$sig$'
+               : '');
 
     $versions = array(
         'default' => array(
@@ -13,7 +16,7 @@
             'message'   => 'First minor release of FanPress CM 3.x, includes bugfixes and new features!',
             'notice'    => 'http://nobody-knows.org/fanpress-cm-3-1-0-veroeffentlicht/',
             'signature' => $signature
-        ),
+        )        
 
 //        '3.1.0-rc5dev' => array(
 //            'file'      => 'http://nobody-knows.org/updatepools/fanpress/system/packages/fanpress_update_version3.1.0.zip',
