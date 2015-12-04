2d5e7b0da4092d83dd2f6acc234e2b12f00b151b
Stefan <sea75300@yahoo.de>
2015-12-04 21:05:05 +0100

    Release 3.1.5

--- internal/userver/system/versionlist3.php
+++ internal/userver/system/versionlist3.php
@@ -1,35 +1,35 @@
 <?php
-    $newversion    = '3.1.4';
-    $newversionDev = '3.1.4';
+    $newversion    = '3.1.5';
+    $newversionDev = '3.1.5';
     
-    $installfile   = 'http://nobody-knows.org/updatepools/fanpress/system/packages/fanpress3.1.4_full.zip';
+    $installfile   = 'http://nobody-knows.org/updatepools/fanpress/system/packages/fanpress3.1.5_full.zip';
     
-    $signature = (file_exists(__DIR__.'/packages/fanpress_update_version3.1.4.zip')
-               ? '$sig$'.md5_file(__DIR__.'/packages/fanpress_update_version3.1.4.zip').'_'
-                        .sha1_file(__DIR__.'/packages/fanpress_update_version3.1.4.zip').'$sig$'
+    $signature = (file_exists(__DIR__.'/packages/fanpress_update_version3.1.5.zip')
+               ? '$sig$'.md5_file(__DIR__.'/packages/fanpress_update_version3.1.5.zip').'_'
+                        .sha1_file(__DIR__.'/packages/fanpress_update_version3.1.5.zip').'$sig$'
                : '');
 
     $versions = array(
         'default' => array(
-            'file'      => 'http://nobody-knows.org/updatepools/fanpress/system/packages/fanpress_update_version3.1.4.zip',
+            'file'      => 'http://nobody-knows.org/updatepools/fanpress/system/packages/fanpress_update_version3.1.5.zip',
             'force'     => 0,
-            'message'   => 'Second release of FanPress CM 3.1.x!',
-            'notice'    => 'http://nobody-knows.org/fanpress-cm-3-1-4-veroeffentlicht/',
+            'message'   => 'Third bugfix release of FanPress CM 3.1.x!',
+            'notice'    => 'http://nobody-knows.org/fanpress-cm-3-1-5-veroeffentlicht/',
             'signature' => $signature
         ),
 
         '3.1.1dev' => array(
-            'file'      => 'http://nobody-knows.org/updatepools/fanpress/system/packages/fanpress_update_version3.1.4.zip',
+            'file'      => 'http://nobody-knows.org/updatepools/fanpress/system/packages/fanpress_update_version3.1.5.zip',
             'force'     => 0,
-            'message'   => 'Second release of FanPress CM 3.1.x!',
+            'message'   => 'Third bugfix of FanPress CM 3.1.x!',
             'notice'    => 'http://nobody-knows.org/fanpress-cm-3-1-4-veroeffentlicht/',
             'signature' => ''
         ),
 
         '3.1.3dev' => array(
-            'file'      => 'http://nobody-knows.org/updatepools/fanpress/system/packages/fanpress_update_version3.1.4.zip',
+            'file'      => 'http://nobody-knows.org/updatepools/fanpress/system/packages/fanpress_update_version3.1.5.zip',
             'force'     => 0,
-            'message'   => 'Second release of FanPress CM 3.1.x!',
+            'message'   => 'Third bugfix of FanPress CM 3.1.x!',
             'notice'    => 'http://nobody-knows.org/fanpress-cm-3-1-4-veroeffentlicht/',
             'signature' => ''
         )
