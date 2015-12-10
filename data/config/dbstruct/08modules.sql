44e1c5068142a1f4f2f1f89c9040241e22afb062
Stefan <sea75300@yahoo.de>
2015-12-10 22:56:08 +0100

    dbStruct von /data/config nach /data/dbStruct verschieben

--- data/config/dbstruct/08modules.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `{{dbpref}}_modules` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `modkey` varchar(512) NOT NULL,
-  `version` varchar(64) NOT NULL,
-  `status` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
