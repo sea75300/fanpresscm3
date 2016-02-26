2e9677557308b98d7b535c3e130e951101840ff1
Stefan <sea75300@yahoo.de>
2016-02-26 20:29:48 +0100

    FPCM 3.2 beta 1

--- internal/userver/system/versionlist3.php
+++ internal/userver/system/versionlist3.php
@@ -1,6 +1,6 @@
 <?php
     $newversion    = '3.1.5';
-    $newversionDev = '3.1.5';
+    $newversionDev = '3.2.0-b1';
     
     $installfile   = 'http://nobody-knows.org/updatepools/fanpress/system/packages/fanpress3.1.5_full.zip';
     
@@ -18,18 +18,18 @@
             'signature' => $signature
         ),
 
-        '3.1.1dev' => array(
-            'file'      => 'http://nobody-knows.org/updatepools/fanpress/system/packages/fanpress_update_version3.1.5.zip',
+        '3.2.0-dev' => array(
+            'file'      => 'http://nobody-knows.org/updatepools/fanpress/system/packages/fanpress_update_version3.2.0-b1.zip',
             'force'     => 0,
-            'message'   => 'Third bugfix of FanPress CM 3.1.x!',
+            'message'   => 'FanPress CM 3.2 beta1',
             'notice'    => 'http://nobody-knows.org/fanpress-cm-3-1-4-veroeffentlicht/',
             'signature' => ''
         ),
 
-        '3.1.3dev' => array(
-            'file'      => 'http://nobody-knows.org/updatepools/fanpress/system/packages/fanpress_update_version3.1.5.zip',
+        '3.2.0-a1' => array(
+            'file'      => 'http://nobody-knows.org/updatepools/fanpress/system/packages/fanpress_update_version3.2.0-b1.zip',
             'force'     => 0,
-            'message'   => 'Third bugfix of FanPress CM 3.1.x!',
+            'message'   => 'FanPress CM 3.2 beta1',
             'notice'    => 'http://nobody-knows.org/fanpress-cm-3-1-4-veroeffentlicht/',
             'signature' => ''
         )
