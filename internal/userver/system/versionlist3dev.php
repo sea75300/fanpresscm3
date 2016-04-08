29b76207d9c7df4431239db37806d63c704475e0
Stefan <sea75300@yahoo.de>
2016-04-08 19:44:15 +0200

    FPCM 3.2-rc2

--- internal/userver/system/versionlist3dev.php
+++ internal/userver/system/versionlist3dev.php
@@ -1,13 +1,13 @@
 <?php
-    $newversion = '3.2.0-rc1';
+    $newversion = '3.2.0-rc2';
     
-    $installfile   = 'http://nobody-knows.org/updatepools/fanpress/system/packages/fanpress3.2.0-rc1_full.zip';
+    $installfile   = 'http://nobody-knows.org/updatepools/fanpress/system/packages/fanpress3.2.0-rc2_full.zip';
 
     $versions = array(
         'default' => array(
-            'file'      => 'http://nobody-knows.org/updatepools/fanpress/system/packages/fanpress_update_version3.2.0-rc1.zip',
+            'file'      => 'http://nobody-knows.org/updatepools/fanpress/system/packages/fanpress_update_version3.2.0-rc2.zip',
             'force'     => 0,
-            'message'   => 'First release candidate of FPCM 3.2.0!',
+            'message'   => 'Second release candidate of FPCM 3.2.0!',
             'notice'    => 'http://nobody-knows.org/fanpress-cm-3-2-0-veroeffentlicht/',
             'signature' => ''
         )
