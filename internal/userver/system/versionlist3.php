a06cfebbf7e333a4d60e8b0c6d4279f78a778c02
stefan <stefan@stefan-pc>
2016-06-03 23:37:09 +0200

    Release 3.2.2

--- internal/userver/system/versionlist3.php
+++ internal/userver/system/versionlist3.php
@@ -1,19 +1,19 @@
 <?php
-    $newversion    = '3.2.1';
+    $newversion    = '3.2.2';
     
-    $installfile   = 'http://updates.nobody-knows.org/fanpress/system/packages/fanpress3.2.1_full.zip';
+    $installfile   = 'http://updates.nobody-knows.org/fanpress/system/packages/fanpress3.2.2_full.zip';
     
-    $signature = (file_exists(__DIR__.'/packages/fanpress_update_version3.2.1.zip')
-               ? '$sig$'.md5_file(__DIR__.'/packages/fanpress_update_version3.2.1.zip').'_'
-                        .sha1_file(__DIR__.'/packages/fanpress_update_version3.2.1.zip').'$sig$'
+    $signature = (file_exists(__DIR__.'/packages/fanpress_update_version3.2.2.zip')
+               ? '$sig$'.md5_file(__DIR__.'/packages/fanpress_update_version3.2.2.zip').'_'
+                        .sha1_file(__DIR__.'/packages/fanpress_update_version3.2.2.zip').'$sig$'
                : '');
 
     $versions = array(
         'default' => array(
-            'file'      => 'http://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.2.1.zip',
+            'file'      => 'http://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.2.2.zip',
             'force'     => 0,
             'message'   => 'Next minor release of FanPress CM 3.x!',
-            'notice'    => 'http://nobody-knows.org/fanpress-cm-3-2-1-veroeffentlicht/',
+            'notice'    => 'http://nobody-knows.org/fanpress-cm-3-2-2-veroeffentlicht/',
             'signature' => $signature
         )
     );
--- internal/userver/system/versionlist3dev.php
+++ internal/userver/system/versionlist3dev.php
@@ -1,14 +1,14 @@
 <?php
-    $newversion = '3.2.1';
+    $newversion = '3.2.2';
     
-    $installfile   = 'http://nobody-knows.org/updatepools/fanpress/system/packages/fanpress3.2.1_full.zip';
+    $installfile   = 'http://nobody-knows.org/updatepools/fanpress/system/packages/fanpress3.2.2_full.zip';
 
     $versions = array(
         'default' => array(
-            'file'      => 'http://nobody-knows.org/updatepools/fanpress/system/packages/fanpress_update_version3.2.1.zip',
+            'file'      => 'http://nobody-knows.org/updatepools/fanpress/system/packages/fanpress_update_version3.2.2.zip',
             'force'     => 0,
             'message'   => 'Next minor release of FanPress CM 3.x!',
-            'notice'    => 'http://nobody-knows.org/fanpress-cm-3-2-1-veroeffentlicht/',
+            'notice'    => 'http://nobody-knows.org/fanpress-cm-3-2-2-veroeffentlicht/',
             'signature' => ''
         )
     );
