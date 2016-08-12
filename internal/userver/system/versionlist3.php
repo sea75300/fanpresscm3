de050a745066ff7e3f3bda121f85d5b1dd85bb96
stefan <stefan@stefan-pc>
2016-08-12 19:03:17 +0200

    HTTPS-Umstellung für FPCM 3.2.x.

--- internal/userver/system/versionlist3.php
+++ internal/userver/system/versionlist3.php
@@ -1,19 +1,19 @@
 <?php
-    $newversion    = '3.2.3';
+    $newversion    = '3.2.4';
     
-    $installfile   = 'http://updates.nobody-knows.org/fanpress/system/packages/fanpress3.2.3_full.zip';
+    $installfile   = 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress3.2.4_full.zip';
     
-    $signature = (file_exists(__DIR__.'/packages/fanpress_update_version3.2.3.zip')
-               ? '$sig$'.md5_file(__DIR__.'/packages/fanpress_update_version3.2.3.zip').'_'
-                        .sha1_file(__DIR__.'/packages/fanpress_update_version3.2.3.zip').'$sig$'
+    $signature = (file_exists(__DIR__.'/packages/fanpress_update_version3.2.4.zip')
+               ? '$sig$'.md5_file(__DIR__.'/packages/fanpress_update_version3.2.4.zip').'_'
+                        .sha1_file(__DIR__.'/packages/fanpress_update_version3.2.4.zip').'$sig$'
                : '');
 
     $versions = array(
         'default' => array(
-            'file'      => 'http://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.2.3.zip',
+            'file'      => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.2.4.zip',
             'force'     => 0,
             'message'   => 'Next minor release of FanPress CM 3.x!',
-            'notice'    => 'http://nobody-knows.org/fanpress-cm-3-2-3-veroeffentlicht/',
+            'notice'    => 'https://nobody-knows.org/fanpress-cm-3-2-3-veroeffentlicht/',
             'signature' => $signature
         )
     );
