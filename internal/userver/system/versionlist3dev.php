deb64a77c0a82b02d2ce8569beb83cb766564503
stefan <stefan@stefan-pc>
2016-08-22 19:29:22 +0200

    Versionsnummer falsch

--- internal/userver/system/versionlist3dev.php
+++ internal/userver/system/versionlist3dev.php
@@ -1,11 +1,11 @@
 <?php
 
-    $installfile   = 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress3-3-0_full.zip';
+    $installfile   = 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress3.3.0_full.zip';
 
     $versions = array(
         'default' => array(
-            'newversion'    => '3-3-0',
-            'file'          => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3-3-0.zip',
+            'newversion'    => '3.3.0',
+            'file'          => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.3.0.zip',
             'force'         => 0,
             'message'       => 'Third minor version of FanPress CM 3.x!',
             'notice'        => 'https://nobody-knows.org/fanpress-cm-3-3-0-veroeffentlicht/',
