17f27ac7b0a439d77b04e2b2dd8bf0912537e994
sea75300 <stefan@stefan-pc>
2016-10-28 20:18:17 +0200

    Release v3.3.3

--- internal/userver/system/versionlist3dev.php
+++ internal/userver/system/versionlist3dev.php
@@ -1,14 +1,14 @@
 <?php
 
-    $installfile   = 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress3.3.2_full.zip';
+    $installfile   = 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress3.3.3_full.zip';
 
     $versions = array(
         'default' => array(
-            'newversion'    => '3.3.2',
-            'file'          => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.3.2.zip',
+            'newversion'    => '3.3.3',
+            'file'          => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.3.3.zip',
             'force'         => 0,
-            'message'       => 'Second update for FanPress CM 3.3.2! Introduces a few new functions to databse layer and bugfixes.',
-            'notice'        => 'https://nobody-knows.org/fanpress-cm-3-3-2-veroeffentlicht/',
+            'message'       => 'Second update for FanPress CM 3.3.3! Introduces a few new functions to databse layer and bugfixes.',
+            'notice'        => 'https://nobody-knows.org/fanpress-cm-3-3-3-veroeffentlicht/',
             'signature'     => ''
         )
     );
