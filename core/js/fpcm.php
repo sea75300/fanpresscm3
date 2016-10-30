5a6bb3b5da9aa02ce787606fd267b6103dbcaeee
stefan <stefan@stefan-pc>
2016-10-30 15:09:57 +0100

    Standard-JS-Files werden kombiniert geladen

---
+++ core/js/fpcm.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Combined JavaScript Files
+ * @author Stefan Seehafer <sea75300@yahoo.de>
+ * @copyright (c) 2011-2016, Stefan Seehafer
+ * @license http://www.gnu.org/licenses/gpl.txt GPLv3
+ */
+
+require_once dirname(dirname(__DIR__)).'/inc/common.php';
+
+$data = array('content' => '', 'filesize' => 0);
+$cache = new \fpcm\classes\cache('jsfiles');
+
+if ($cache->isExpired()) {
+
+    $jsFiles = array(
+        __DIR__.'/ui.js',
+        __DIR__.'/ajax.js',
+        __DIR__.'/functions.js',
+        __DIR__.'/system.js'
+    );
+
+    foreach ($jsFiles as $jsFile) {
+
+        $fileContent = file_get_contents($jsFile).PHP_EOL.PHP_EOL;
+        if (!$fileContent) {
+            continue;
+        }
+
+        $data['content'] .= $fileContent;
+        $data['filesize'] = filesize($jsFile);
+
+    }
+
+    $cache->write($data);
+} else {
+    $data = $cache->read();
+}
+
+header("Content-Type: application/javascript");
+header("Content-Length: ".$data['filesize']);
+die($data['content']);
