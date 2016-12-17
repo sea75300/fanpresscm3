5a4fe38d80e887c88fe12dcb536160f487de7f0a
stefan <stefan@stefan-pc>
2016-12-17 17:29:34 +0100

    Module list dummy

--- internal/userver/modules/modulelist3.php
+++ internal/userver/modules/modulelist3.php
@@ -3,14 +3,86 @@
     $maxVersion = "4.0.0";
 
     $modulesServer = array(
-        'nkorg/???' => array(
-            'name'          => '???',
-            'description'   => '??',
-            'author'        => 'imagine',
-            'link'          => 'https://nobody-knows.org/',
-            'version'       => '???',
-            'minsysverion'  => '3.1.0',
-            'maxsysverion'  => '4.0.0'
-        )
+//        'nkorg/classicimporter' => array(
+//            'name'          => 'FanPress CM Classic Importer',
+//            'description'   => 'Import module for FanPress CM 2.5',
+//            'author'        => 'imagine',
+//            'link'          => 'https://nobody-knows.org/',
+//            'version'       => '3.4.0',
+//            'minsysverion'  => '3.4.0',
+//            'maxsysverion'  => '4.0.0'
+//        ),
+//        'nkorg/inactivity_manager' => array(
+//            'name'          => 'Inactivity Manager',
+//            'description'   => 'The Inactivity Manager module allows you to manage and display of various messages, in case of temporary inactivity or increased response time of your website, for example when you\'re on vacation. The module also allows you to disable certain features of your FanPress CM system like posting new comments.',
+//            'author'        => 'imagine',
+//            'link'          => 'https://nobody-knows.org/',
+//            'version'       => '3.4.0',
+//            'minsysverion'  => '3.4.0',
+//            'maxsysverion'  => '4.0.0'
+//        ),
+//        'nkorg/integration' => array(
+//            'name'          => 'FanPress CM Integration',
+//            'description'   => 'Helper module for integration of FanPress CM into your website.',
+//            'author'        => 'imagine',
+//            'link'          => 'https://nobody-knows.org/',
+//            'version'       => '3.4.0',
+//            'minsysverion'  => '3.4.0',
+//            'maxsysverion'  => '4.0.0'
+//        ),
+//        'nkorg/langeditor' => array(
+//            'name'          => 'Language Editor',
+//            'description'   => 'The "Language editor" module offers you an way to easily edit language files included in FanPress CM as is and installed modules.',
+//            'author'        => 'imagine',
+//            'link'          => 'https://nobody-knows.org/',
+//            'version'       => '3.4.0',
+//            'minsysverion'  => '3.4.0',
+//            'maxsysverion'  => '4.0.0'
+//        ),
+//        'nkorg/modulecreator' => array(
+//            'name'          => 'FanPress CM 3.x Modulecreator Module',
+//            'description'   => 'This module allows you to create a base structure for a new module including events, config files, folder structure and so on.',
+//            'author'        => 'imagine',
+//            'link'          => 'https://nobody-knows.org/',
+//            'version'       => '3.4.0',
+//            'minsysverion'  => '3.4.0',
+//            'maxsysverion'  => '4.0.0'
+//        ),
+//        'nkorg/rssimport' => array(
+//            'name'          => 'RSS Importer',
+//            'description'   => 'Importer for article feeds based on RSS2 or Atom feeds.',
+//            'author'        => 'imagine',
+//            'link'          => 'https://nobody-knows.org/',
+//            'version'       => '3.4.0',
+//            'minsysverion'  => '3.4.0',
+//            'maxsysverion'  => '4.0.0'
+//        ),
+//        'nkorg/sitemaplinklist' => array(
+//            'name'          => 'sitemap.xml Link List',
+//            'description'   => 'Module for use with sitemap.xml files to fill link list in TinyMCE or Link auto completion in HTML-Editor with internal link.',
+//            'author'        => 'imagine',
+//            'link'          => 'https://nobody-knows.org/',
+//            'version'       => '3.4.0',
+//            'minsysverion'  => '3.4.0',
+//            'maxsysverion'  => '4.0.0'
+//        ),
+//        'nkorg/support' => array(
+//            'name'          => 'FanPress CM Support Module',
+//            'description'   => 'This module is a helper module for support cases. It will add a new user account to your system on setup. Only install this module when you are asked to do so!',
+//            'author'        => 'imagine',
+//            'link'          => 'https://nobody-knows.org/',
+//            'version'       => '3.4.0',
+//            'minsysverion'  => '3.4.0',
+//            'maxsysverion'  => '4.0.0'
+//        ),
+//        'nkorg/tweetextender' => array(
+//            'name'          => 'Tweet Extender',
+//            'description'   => 'The Tweet Extender allows you to modify posts send to twitter when the connection and sending on creation/ updating is enabled.',
+//            'author'        => 'imagine',
+//            'link'          => 'https://nobody-knows.org/',
+//            'version'       => '3.4.0',
+//            'minsysverion'  => '3.4.0',
+//            'maxsysverion'  => '4.0.0'
+//        ),
     );
 ?>
