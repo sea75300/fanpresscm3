9707bedce21c4232f399eb2a72647cfa564ed0da
stefan <stefan@stefan-pc>
2017-01-06 20:46:11 +0100

    Update TinyMCE 4.5.2

--- internal/userver/system/versionlist3dev.php
+++ internal/userver/system/versionlist3dev.php
@@ -1,11 +1,11 @@
 <?php
 
-    $installfile   = 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress3.4.1_full.zip';
+    $installfile   = 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress3.4.2_full.zip';
 
     $versions = array(
         'default' => array(
-            'newversion'    => '3.4.1',
-            'file'          => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.4.1.zip',
+            'newversion'    => '3.4.2',
+            'file'          => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.4.2.zip',
             'force'         => 0,
             'message'       => 'Third minor version of FanPress CM 3.x!',
             'notice'        => 'https://nobody-knows.org/fanpress-cm-3-4-0-veroeffentlicht/',
