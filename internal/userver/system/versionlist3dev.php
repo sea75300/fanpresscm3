0495067c83c28820d401e0b4e9b860aaa91c20ff
stefan <stefan@stefan-pc>
2017-02-03 20:29:47 +0100

    Release FPCm 3.4.4

--- internal/userver/system/versionlist3dev.php
+++ internal/userver/system/versionlist3dev.php
@@ -1,11 +1,11 @@
 <?php
 
-    $installfile   = 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress3.4.3_full.zip';
+    $installfile   = 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress3.4.4_full.zip';
 
     $versions = array(
         'default' => array(
-            'newversion'    => '3.4.3',
-            'file'          => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.4.3.zip',
+            'newversion'    => '3.4.4',
+            'file'          => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.4.4.zip',
             'force'         => 0,
             'message'       => 'Third minor version of FanPress CM 3.x!',
             'notice'        => 'https://nobody-knows.org/fanpress-cm-3-4-0-veroeffentlicht/',
