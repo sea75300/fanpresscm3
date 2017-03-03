442de1f2c01f89920af7630f47be726f7b9dcbde
stefan <stefan@stefan-pc>
2017-03-03 19:57:53 +0100

    3.5.0-b1

--- internal/userver/system/versionlist3dev.php
+++ internal/userver/system/versionlist3dev.php
@@ -1,11 +1,11 @@
 <?php
 
-    $installfile   = 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress3.5.0-rc1_full.zip';
+    $installfile   = 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress3.5.0-b1-rc1_full.zip';
 
     $versions = array(
         'default' => array(
-            'newversion'    => '3.5.0-rc1',
-            'file'          => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.5.0-rc1.zip',
+            'newversion'    => '3.5.0-b1-rc1',
+            'file'          => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.5.0-b1-rc1.zip',
             'force'         => 0,
             'message'       => 'Third minor version of FanPress CM 3.x!',
             'notice'        => 'https://nobody-knows.org/fanpress-cm-3-4-0-veroeffentlicht/',
