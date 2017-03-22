15b094c5d17ae84f3ef59a44fc67a3e2caf2e3a7
stefan <stefan@stefan-pc>
2017-03-22 23:07:54 +0100

    FPCm 3.5.0-b4

--- internal/userver/system/versionlist3dev.php
+++ internal/userver/system/versionlist3dev.php
@@ -1,14 +1,14 @@
 <?php
 
-    $installfile   = 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress3.5.0-b3_full.zip';
+        $installfile   = 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress3.5.0-b4_full.zip';
 
     $versions = array(
         'default' => array(
-            'newversion'    => '3.5.0-b3',
-            'file'          => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.5.0-b3.zip',
+            'newversion'    => '3.5.0-b4',
+            'file'          => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.5.0-b4.zip',
             'force'         => 0,
-            'message'       => 'Third minor version of FanPress CM 3.x!',
-            'notice'        => 'https://nobody-knows.org/fanpress-cm-3-4-0-veroeffentlicht/',
+            'message'       => '5th minor version of FanPress CM 3.x!',
+            'notice'        => 'https://nobody-knows.org/fanpress-cm-3-5-0-veroeffentlicht/',
             'signature'     => '',
             'phpversion'    => '5.5.3'
         )
