f4fda14c318275786f7517dac171feae868c177e
stefan <stefan@stefan-laptop>
2017-04-12 22:22:29 +0200

    System Stats: Icons

--- inc/model/dashboard/sysstats.php
+++ inc/model/dashboard/sysstats.php
@@ -67,52 +67,52 @@
         protected function getArticleStats() {
             $articleList = new \fpcm\model\articles\articlelist();
             
-            $this->tableContent[] = '<tr><td><strong>'.$this->language->translate('SYSTEM_STATS_ARTICLES_ALL').':</strong></td><td class="fpcm-ui-center">'.$articleList->countArticlesByCondition().'</td></tr>';
+            $this->tableContent[] = '<tr><td><span class="fa fa-book fa-fw"></span> <strong>'.$this->language->translate('SYSTEM_STATS_ARTICLES_ALL').':</strong></td><td class="fpcm-ui-center">'.$articleList->countArticlesByCondition().'</td></tr>';
             
             $sObj = new \fpcm\model\articles\search();
             $sObj->active = 1;
-            $this->tableContent[] = '<tr><td><strong>'.$this->language->translate('SYSTEM_STATS_ARTICLES_ACTIVE').':</strong></td><td class="fpcm-ui-center">'.$articleList->countArticlesByCondition($sObj).'</td></tr>';
+            $this->tableContent[] = '<tr><td><span class="fa fa-newspaper-o fa-fw"></span> <strong>'.$this->language->translate('SYSTEM_STATS_ARTICLES_ACTIVE').':</strong></td><td class="fpcm-ui-center">'.$articleList->countArticlesByCondition($sObj).'</td></tr>';
 
             $sObj = new \fpcm\model\articles\search();
             $sObj->archived = 1;
-            $this->tableContent[] = '<tr><td><strong>'.$this->language->translate('SYSTEM_STATS_ARTICLES_ARCHIVE').':</strong></td><td class="fpcm-ui-center">'.$articleList->countArticlesByCondition($sObj).'</td></tr>';
+            $this->tableContent[] = '<tr><td><span class="fa fa-archive fa-fw"></span> <strong>'.$this->language->translate('SYSTEM_STATS_ARTICLES_ARCHIVE').':</strong></td><td class="fpcm-ui-center">'.$articleList->countArticlesByCondition($sObj).'</td></tr>';
 
             $sObj = new \fpcm\model\articles\search();
             $sObj->drafts = 1;
-            $this->tableContent[] = '<tr><td><strong>'.$this->language->translate('SYSTEM_STATS_ARTICLES_DRAFT').':</strong></td><td class="fpcm-ui-center">'.$articleList->countArticlesByCondition($sObj).'</td></tr>';
+            $this->tableContent[] = '<tr><td><span class="fa fa-file-text-o fa-fw"></span> <strong>'.$this->language->translate('SYSTEM_STATS_ARTICLES_DRAFT').':</strong></td><td class="fpcm-ui-center">'.$articleList->countArticlesByCondition($sObj).'</td></tr>';
 
             $sObj = new \fpcm\model\articles\search();
             $sObj->deleted = 1;
-            $this->tableContent[] = '<tr><td><strong>'.$this->language->translate('SYSTEM_STATS_ARTICLES_TRASH').':</strong></td><td class="fpcm-ui-center">'.$articleList->countArticlesByCondition($sObj).'</td></tr>';
+            $this->tableContent[] = '<tr><td><span class="fa fa-trash-o fa-fw"></span> <strong>'.$this->language->translate('SYSTEM_STATS_ARTICLES_TRASH').':</strong></td><td class="fpcm-ui-center">'.$articleList->countArticlesByCondition($sObj).'</td></tr>';
 
             $sObj = new \fpcm\model\articles\search();
             $sObj->approval = 1;
             $count = $articleList->countArticlesByCondition($sObj);
-            $this->tableContent[] = '<tr class="'.($count > 0 ? 'fpcm-ui-important-text' : 'fpcm-ui-text').'"><td><strong>'.$this->language->translate('SYSTEM_STATS_ARTICLES_APPROVAL').':</strong></td><td class="fpcm-ui-center">'.$count.'</td></tr>';
+            $this->tableContent[] = '<tr class="'.($count > 0 ? 'fpcm-ui-important-text' : 'fpcm-ui-text').'"><td><span class="fa fa-thumbs-o-up fa-fw"></span><strong>'.$this->language->translate('SYSTEM_STATS_ARTICLES_APPROVAL').':</strong></td><td class="fpcm-ui-center">'.$count.'</td></tr>';
         }
-        
+
         /**
          * Kommentar-Statistiken berechnen
          */
         protected function getCommentStats() {
             $commentList = new \fpcm\model\comments\commentList();
             
-            $this->tableContent[] = '<tr><td><strong>'.$this->language->translate('SYSTEM_STATS_COMMENTS_ALL').':</strong></td><td class="fpcm-ui-center">'.$commentList->countCommentsByCondition().'</td></tr>';
+            $this->tableContent[] = '<tr><td><span class="fa fa-comments fa-fw"></span> <strong>'.$this->language->translate('SYSTEM_STATS_COMMENTS_ALL').':</strong></td><td class="fpcm-ui-center">'.$commentList->countCommentsByCondition().'</td></tr>';
 
             $sObj = new \fpcm\model\comments\search();
             $sObj->unapproved = true;
             $count = $commentList->countCommentsByCondition($sObj);
-            $this->tableContent[] = '<tr class="'.($count > 0 ? 'fpcm-ui-important-text' : 'fpcm-ui-text').'"><td><strong>'.$this->language->translate('SYSTEM_STATS_COMMENTS_UNAPPR').':</strong></td><td class="fpcm-ui-center">'.$count.'</td></tr>';
+            $this->tableContent[] = '<tr class="'.($count > 0 ? 'fpcm-ui-important-text' : 'fpcm-ui-text').'"><td><span class="fa fa-check-circle-o fa-fw"></span> <strong>'.$this->language->translate('SYSTEM_STATS_COMMENTS_UNAPPR').':</strong></td><td class="fpcm-ui-center">'.$count.'</td></tr>';
             
             $sObj = new \fpcm\model\comments\search();
             $sObj->private = true;
             $count = $commentList->countCommentsByCondition($sObj);
-            $this->tableContent[] = '<tr class="'.($count > 0 ? 'fpcm-ui-important-text' : 'fpcm-ui-text').'"><td><strong>'.$this->language->translate('SYSTEM_STATS_COMMENTS_PRIVATE').':</strong></td><td class="fpcm-ui-center">'.$count.'</td></tr>';
+            $this->tableContent[] = '<tr class="'.($count > 0 ? 'fpcm-ui-important-text' : 'fpcm-ui-text').'"><td><span class="fa fa-eye-slash fa-fw"></span> <strong>'.$this->language->translate('SYSTEM_STATS_COMMENTS_PRIVATE').':</strong></td><td class="fpcm-ui-center">'.$count.'</td></tr>';
             
             $sObj = new \fpcm\model\comments\search();
             $sObj->spam = true;
             $count = $commentList->countCommentsByCondition($sObj);
-            $this->tableContent[] = '<tr class="'.($count > 0 ? 'fpcm-ui-important-text' : 'fpcm-ui-text').'"><td><strong>'.$this->language->translate('SYSTEM_STATS_COMMENTS_SPAM').':</strong></td><td class="fpcm-ui-center">'.$count.'</td></tr>';
+            $this->tableContent[] = '<tr class="'.($count > 0 ? 'fpcm-ui-important-text' : 'fpcm-ui-text').'"><td><span class="fa fa-flag fa-fw"></span> <strong>'.$this->language->translate('SYSTEM_STATS_COMMENTS_SPAM').':</strong></td><td class="fpcm-ui-center">'.$count.'</td></tr>';
         }
         
         /**
@@ -122,10 +122,10 @@
             
             $userCountAll = $this->dbcon->count(\fpcm\classes\database::tableAuthors);
             $userCountAct = $this->dbcon->count(\fpcm\classes\database::tableAuthors, '*', 'disabled = 0');            
-            $this->tableContent[] = '<tr><td><strong>'.$this->language->translate('SYSTEM_STATS_USERS').':</strong></td><td class="fpcm-ui-center">'.$userCountAll.' ('.$userCountAct.')</td></tr>';
+            $this->tableContent[] = '<tr><td><span class="fa fa-users fa-fw"></span> <strong>'.$this->language->translate('SYSTEM_STATS_USERS').':</strong></td><td class="fpcm-ui-center">'.$userCountAll.' ('.$userCountAct.')</td></tr>';
             
             $categoryCount = $this->dbcon->count(\fpcm\classes\database::tableCategories);
-            $this->tableContent[] = '<tr><td><strong>'.$this->language->translate('SYSTEM_STATS_CATEGORIES').':</strong></td><td class="fpcm-ui-center">'.$categoryCount.'</td></tr>';
+            $this->tableContent[] = '<tr><td><span class="fa fa-file-o fa-fw"></span> <strong>'.$this->language->translate('SYSTEM_STATS_CATEGORIES').':</strong></td><td class="fpcm-ui-center">'.$categoryCount.'</td></tr>';
         }
         
         /**
@@ -134,11 +134,11 @@
         protected function getFileStats() {
             
             $fileCount = $this->dbcon->count(\fpcm\classes\database::tableFiles, '*');
-            $this->tableContent[] = '<tr><td><strong>'.$this->language->translate('SYSTEM_STATS_UPLOAD_COUNT').':</strong></td><td class="fpcm-ui-center">'.$fileCount.'</td></tr>'; 
+            $this->tableContent[] = '<tr><td><span class="fa fa-files-o fa-fw"></span> <strong>'.$this->language->translate('SYSTEM_STATS_UPLOAD_COUNT').':</strong></td><td class="fpcm-ui-center">'.$fileCount.'</td></tr>'; 
             
             $imgList    = new \fpcm\model\files\imagelist();
             $folderSize = \fpcm\classes\tools::calcSize($imgList->getUploadFolderSize());
-            $this->tableContent[] = '<tr><td><strong>'.$this->language->translate('SYSTEM_STATS_UPLOAD_SIZE').':</strong></td><td class="fpcm-ui-center">'.$folderSize.'</td></tr>';
+            $this->tableContent[] = '<tr><td><span class="fa fa-calculator fa-fw"></span> <strong>'.$this->language->translate('SYSTEM_STATS_UPLOAD_SIZE').':</strong></td><td class="fpcm-ui-center">'.$folderSize.'</td></tr>';
         }
         
         /**
@@ -146,7 +146,7 @@
          */
         protected function getCacheStats() {
             $folderSize = \fpcm\classes\tools::calcSize($this->cache->getSize());
-            $this->tableContent[] = '<tr><td><strong>'.$this->language->translate('SYSTEM_STATS_CACHE_SIZE').':</strong></td><td class="fpcm-ui-center">'.$folderSize.'</td></tr>';            
+            $this->tableContent[] = '<tr><td><span class="fa fa-recycle fa-fw"></span> <strong>'.$this->language->translate('SYSTEM_STATS_CACHE_SIZE').':</strong></td><td class="fpcm-ui-center">'.$folderSize.'</td></tr>';            
         }
         
         
