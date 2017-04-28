9b49fdbc3f0346efbb3986c826be4841cfdafb6b
stefan <stefan@stefan-pc>
2017-04-28 23:39:54 +0200

    Version to 3.5.1

--- internal/userver/system/versionlist3.php
+++ internal/userver/system/versionlist3.php
@@ -1,16 +1,16 @@
 <?php
 
-    $installfile   = 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress3.5.0_full.zip';
+    $installfile   = 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress3.5.1_full.zip';
     
-    $signature = (file_exists(__DIR__.'/packages/fanpress_update_version3.5.0.zip')
-               ? '$sig$'.md5_file(__DIR__.'/packages/fanpress_update_version3.5.0.zip').'_'
-                        .sha1_file(__DIR__.'/packages/fanpress_update_version3.5.0.zip').'$sig$'
+    $signature = (file_exists(__DIR__.'/packages/fanpress_update_version3.5.1.zip')
+               ? '$sig$'.md5_file(__DIR__.'/packages/fanpress_update_version3.5.1.zip').'_'
+                        .sha1_file(__DIR__.'/packages/fanpress_update_version3.5.1.zip').'$sig$'
                : '');
 
     $versions = array(
         'default' => array(
-            'newversion'    => '3.5.0',
-            'file'      => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.5.0.zip',
+            'newversion'    => '3.5.1',
+            'file'      => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.5.1.zip',
             'force'     => 0,
             'message'   => 'Next minor release of FanPress CM 3.x!',
             'notice'    => 'https://nobody-knows.org/fanpress-cm-3-5-0-veroeffentlicht/',
--- internal/userver/system/versionlist3dev.php
+++ internal/userver/system/versionlist3dev.php
@@ -1,11 +1,11 @@
 <?php
 
-        $installfile   = 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress3.5.0_full.zip';
+        $installfile   = 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress3.5.1_full.zip';
 
     $versions = array(
         'default' => array(
-            'newversion'    => '3.5.0',
-            'file'          => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.5.0.zip',
+            'newversion'    => '3.5.1',
+            'file'          => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.5.1.zip',
             'force'         => 0,
             'message'       => '5th minor version of FanPress CM 3.x!',
             'notice'        => 'https://nobody-knows.org/fanpress-cm-3-5-0-veroeffentlicht/',
