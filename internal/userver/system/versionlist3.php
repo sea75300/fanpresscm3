4a94d1568c503070924f9789902d936e73a50eb9
stefan <stefan@stefan-pc>
2017-05-22 14:33:32 +0200

    Release 3.5.2

--- internal/userver/system/versionlist3.php
+++ internal/userver/system/versionlist3.php
@@ -1,16 +1,16 @@
 <?php
 
-    $installfile   = 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress3.5.1-pl1_full.zip';
+    $installfile   = 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress3.5.2_full.zip';
     
-    $signature = (file_exists(__DIR__.'/packages/fanpress_update_version3.5.1-pl1.zip')
-               ? '$sig$'.md5_file(__DIR__.'/packages/fanpress_update_version3.5.1-pl1.zip').'_'
-                        .sha1_file(__DIR__.'/packages/fanpress_update_version3.5.1-pl1.zip').'$sig$'
+    $signature = (file_exists(__DIR__.'/packages/fanpress_update_version3.5.2.zip')
+               ? '$sig$'.md5_file(__DIR__.'/packages/fanpress_update_version3.5.2.zip').'_'
+                        .sha1_file(__DIR__.'/packages/fanpress_update_version3.5.2.zip').'$sig$'
                : '');
 
     $versions = array(
         'default' => array(
-            'newversion'    => '3.5.1-pl1',
-            'file'      => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.5.1-pl1.zip',
+            'newversion'    => '3.5.2',
+            'file'      => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.5.2.zip',
             'force'     => 0,
             'message'   => 'Next minor release of FanPress CM 3.x!',
             'notice'    => 'https://nobody-knows.org/fanpress-cm-3-5-0-veroeffentlicht/',
