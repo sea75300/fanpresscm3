159ca1b6c2976f9e828255e2a3b32674f9a53070
stefan <stefan@stefan-pc>
2017-12-26 14:41:48 +0100

    * Style-Bugfix TinyMCE * Release FPCm 3.6.3

--- internal/userver/system/versionlist3.php
+++ internal/userver/system/versionlist3.php
@@ -1,31 +1,30 @@
 <?php
 
-    $installfile   = 'https://github.com/sea75300/fanpresscm3/releases/download/v3.6.2/fanpress3.6.2_full.zip';
+    $installfile    = 'https://github.com/sea75300/fanpresscm3/releases/download/v3.6.3/fanpress3.6.3_full.zip';
     
-    $signature = (file_exists(__DIR__.'/packages/fanpress_update_version3.6.2.zip')
-               ? '$sig$'.md5_file(__DIR__.'/packages/fanpress_update_version3.6.2.zip').'_'
-                        .sha1_file(__DIR__.'/packages/fanpress_update_version3.6.2.zip').'$sig$'
-               : '');
+    $signature     = (file_exists(__DIR__.'/packages/fanpress_update_version3.6.3.zip')
+                    ? '$sig$'.md5_file(__DIR__.'/packages/fanpress_update_version3.6.3.zip').'_'.sha1_file(__DIR__.'/packages/fanpress_update_version3.6.3.zip').'$sig$'
+                    : '');
 
     $versions = array(
         'default' => array(
-            'newversion'    => '3.6.2',
-            'file'      => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.6.2.zip',
+            'newversion'    => '3.6.3',
+            'file'      => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.6.3.zip',
             'force'     => 0,
             'message'   => 'Next minor release of FanPress CM 3.x!',
-            'notice'    => 'https://nobody-knows.org/fanpress-cm-3-6-0-veroeffentlicht/',
+            'notice'    => 'https://nobody-knows.org/fanpress-cm-3-6-3-veroeffentlicht/',
             'signature' => $signature,
             'phpversion' => '5.5.3',
             'release'   => '30.11.2017'
         ),
 
         '3.2' => array(
-            'newversion'    => '3.4.4',
-            'file'      => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.4.4.zip',
+            'newversion'    => '0.0.0',
+            'file'      => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.0.0.zip',
             'force'     => 1,
-            'message'   => 'No direct upgrade from FPCM 3.2.x to FPCVM 3.5.x!',
-            'notice'    => 'https://nobody-knows.org/fanpress-cm-3-4-0-veroeffentlicht/',
-            'signature' => $signature,
+            'message'   => 'Support for FPCM 3.2.x discontinued as of December 26th 2017!',
+            'notice'    => 'https://nobody-knows.org/fanpress-cm-3-6-3-veroeffentlicht/',
+            'signature' => $signature2,
             'phpversion'    => '5.4.0',
             'release'   => '03.02.2017'
         ),
--- internal/userver/system/versionlist3dev.php
+++ internal/userver/system/versionlist3dev.php
@@ -1,14 +1,14 @@
 <?php
 
-        $installfile   = 'https://github.com/sea75300/fanpresscm3/releases/download/v3.6.2/fanpress3.6.2_full.zip';
+        $installfile   = 'https://github.com/sea75300/fanpresscm3/releases/download/v3.6.3/fanpress3.6.3_full.zip';
 
     $versions = array(
         'default' => array(
-            'newversion'    => '3.6.2',
-            'file'          => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.6.2.zip',
+            'newversion'    => '3.6.3',
+            'file'          => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.6.3.zip',
             'force'         => 0,
             'message'       => '5th minor version of FanPress CM 3.x!',
-            'notice'        => 'https://nobody-knows.org/fanpress-cm-3-5-0-veroeffentlicht/',
+            'notice'        => 'https://nobody-knows.org/fanpress-cm-3-6-3-veroeffentlicht/',
             'signature'     => '',
             'phpversion'    => '5.5.3'
         )
