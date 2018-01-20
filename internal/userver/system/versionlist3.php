5619e039b8470c1486afb6d8c40adc9aa5a0ca24
stefan <stefan@stefan-pc>
2018-01-20 12:29:59 +0100

    Release 3.6.4

--- internal/userver/system/versionlist3.php
+++ internal/userver/system/versionlist3.php
@@ -1,15 +1,15 @@
 <?php
 
-    $installfile    = 'https://github.com/sea75300/fanpresscm3/releases/download/v3.6.3/fanpress3.6.3_full.zip';
+    $installfile    = 'https://github.com/sea75300/fanpresscm3/releases/download/v3.6.4/fanpress3.6.4_full.zip';
     
-    $signature     = (file_exists(__DIR__.'/packages/fanpress_update_version3.6.3.zip')
-                    ? '$sig$'.md5_file(__DIR__.'/packages/fanpress_update_version3.6.3.zip').'_'.sha1_file(__DIR__.'/packages/fanpress_update_version3.6.3.zip').'$sig$'
+    $signature     = (file_exists(__DIR__.'/packages/fanpress_update_version3.6.4.zip')
+                    ? '$sig$'.md5_file(__DIR__.'/packages/fanpress_update_version3.6.4.zip').'_'.sha1_file(__DIR__.'/packages/fanpress_update_version3.6.4.zip').'$sig$'
                     : '');
 
     $versions = array(
         'default' => array(
-            'newversion'    => '3.6.3',
-            'file'      => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.6.3.zip',
+            'newversion'    => '3.6.4',
+            'file'      => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.6.4.zip',
             'force'     => 0,
             'message'   => 'Next minor release of FanPress CM 3.x!',
             'notice'    => 'https://nobody-knows.org/fanpress-cm-3-6-3-veroeffentlicht/',
--- internal/userver/system/versionlist3dev.php
+++ internal/userver/system/versionlist3dev.php
@@ -1,11 +1,11 @@
 <?php
 
-        $installfile   = 'https://github.com/sea75300/fanpresscm3/releases/download/v3.6.3/fanpress3.6.3_full.zip';
+        $installfile   = 'https://github.com/sea75300/fanpresscm3/releases/download/v3.6.4/fanpress3.6.4_full.zip';
 
     $versions = array(
         'default' => array(
-            'newversion'    => '3.6.3',
-            'file'          => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.6.3.zip',
+            'newversion'    => '3.6.4',
+            'file'          => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.6.4.zip',
             'force'         => 0,
             'message'       => '5th minor version of FanPress CM 3.x!',
             'notice'        => 'https://nobody-knows.org/fanpress-cm-3-6-3-veroeffentlicht/',
