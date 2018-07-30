12c5d5fe649711cea09e86b84a4c79342561c346
stefan <stefan@stefan-pc>
2018-07-30 16:41:12 +0200

    3.7 beta1

--- internal/userver/system/versionlist3dev.php
+++ internal/userver/system/versionlist3dev.php
@@ -1,11 +1,11 @@
 <?php
 
-        $installfile   = 'https://github.com/sea75300/fanpresscm3/releases/download/v3.6.7/fanpress3.6.7_full.zip';
+    $installfile   = 'https://github.com/sea75300/fanpresscm3/releases/download/v3.7.0-b1/fanpress3.7.0-b1_full.zip';
 
     $versions = array(
         'default' => array(
-            'newversion'    => '3.6.7',
-            'file'          => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.6.7.zip',
+            'newversion'    => '3.7.0-b1',
+            'file'          => 'https://updates.nobody-knows.org/fanpress/system/packages/fanpress_update_version3.7.0-b1.zip',
             'force'         => 0,
             'message'       => '5th minor version of FanPress CM 3.x!',
             'notice'        => 'https://nobody-knows.org/fanpress-cm-3-6-5-veroeffentlicht/',
